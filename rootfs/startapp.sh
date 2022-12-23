dbe471bda634ae13cb2dfbf5db7dd10408ca332c
dkoehler <kontakt@dirkkoehler.com>
2022-12-23 11:59:38 +0100

    comments

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 start_pp() {
-    #change to workdpace dir, so that file open dialog in pp will initially show up with the mounted directory 
+    #change to workdpace dir, so that file open dialog in pp will show up mounted dir in left menu
     cd /opt/portfolio/workspace
     #start application
     exec /opt/portfolio/PortfolioPerformance > /config/pp_out.log 2> /config/pp_err.log
